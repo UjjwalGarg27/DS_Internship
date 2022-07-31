e9dff5c243c160bf5e34c773a7f43c0ce55f3cfb
Ujjwal Garg <gargujjwal2014@gmail.com>
2022-07-31 13:54:50 +0530

    Add files via upload

---
+++ Task - 2 (Data Types)/12.py
@@ -0,0 +1,13 @@
+#Task-12
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+u1 = set(input().split());
+
+b = int(input())
+u2 = set(input().split());
+
+u3 = u1.union(u2)
+
+print(len(u3))
