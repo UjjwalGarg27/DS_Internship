068130a3335a5fd0f552a34d8e5af4634077fc38
Ujjwal Garg <gargujjwal2014@gmail.com>
2022-08-01 17:15:44 +0530

    Add files via upload

---
+++ Task - 4 (Basic Python - Strings)/4.py
@@ -0,0 +1,13 @@
+#Task-4
+
+def mutate_string(string, position, character):
+    n = list(string)
+    n[position] = character
+    string = "".join(n)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
